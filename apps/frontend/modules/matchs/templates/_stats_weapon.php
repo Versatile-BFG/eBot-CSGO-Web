20e722e22099043696c3c119c4e07d62350e7838
versatile-bfg <versatile_bfg@hotmail.com>
2016-04-23 17:53:00 +1000

    quick fix

--- apps/frontend/modules/matchs/templates/_stats_weapon.php
+++ apps/frontend/modules/matchs/templates/_stats_weapon.php
@@ -30,228 +30,229 @@
     <li><a href="#stats-weapon-equipment"><?php echo __("Equipment"); ?></a></li>
 </ul>
 
-<div class="tab-pane" id="stats-weapon-pistols">
-	<table class="table">
-		<thead>
-			<tr>
-				<td rowspan="2"></td>
-				<?php foreach ($pistols as $weapon): ?>
+<div class="tab-content" style="padding-bottom: 10px; margin-bottom: 20px;">
+	<div class="tab-pane active" id="stats-weapon-pistols">
+		<table class="table">
+			<thead>
+				<tr>
+					<td rowspan="2"></td>
+					<?php foreach ($pistols as $weapon): ?>
 					<td style="border-left: 1px solid #DDDDDD; text-align: center; min-width: 50px;" colspan="2"><?php echo image_tag("/images/kills/csgo/" . $weapon . ".png", array("class" => "needTips_S", "title" => $weapon)); ?></td>
-				<?php endforeach; ?>
-			</tr>
-			<tr>
-				<?php foreach ($pistols as $weapon): ?>
+					<?php endforeach; ?>
+				</tr>
+				<tr>
+					<?php foreach ($pistols as $weapon): ?>
 					<td style="border-left: 2px solid #DDDDDD;text-align: center;font-size: 10px; border-right: 1px solid #EEEEEE;">K</td>
 					<td style="font-size: 10px; text-align: center;">D</td>
-				<?php endforeach; ?>
-			</tr>
-		</thead>
-		<tbody>
-			<?php $weaponsStats = array(); ?>
-			<?php foreach ($match->getMap()->getPlayer() as $player): ?>
+					<?php endforeach; ?>
+				</tr>
+			</thead>
+			<tbody>
+				<?php $weaponsStats = array(); ?>
+				<?php foreach ($match->getMap()->getPlayer() as $player): ?>
 				<?php if ($player->getTeam() == "other") continue; ?>
 				<tr>
 					<td style="width: 150px; min-width: 150px;"><a href="<?php echo url_for("player_stats", array("id" => $player->getSteamid())); ?>"><?php echo $player->getPseudo(); ?></a></td>
 					<?php foreach ($pistols as $weapon): ?>
-						<td style="text-align: center;border-left: 2px solid #DDDDDD; border-right: 1px solid #EEEEEE;" <?php if (@$players[$player->getId()][$weapon]["k"] * 1 == 0) echo 'class="muted"'; ?>>
-							<?php echo @$players[$player->getId()][$weapon]["k"] * 1; ?>
-						</td>
-						<td style="text-align: center;" <?php if (@$players[$player->getId()][$weapon]["d"] * 1 == 0) echo 'class="muted"'; ?>>
-							<?php echo @$players[$player->getId()][$weapon]["d"] * 1; ?>
-						</td>
-						<?php @$weaponsStats[$weapon] += @$players[$player->getId()][$weapon]["k"] * 1; ?>
+					<td style="text-align: center;border-left: 2px solid #DDDDDD; border-right: 1px solid #EEEEEE;" <?php if (@$players[$player->getId()][$weapon]["k"] * 1 == 0) echo 'class="muted"'; ?>>
+						<?php echo @$players[$player->getId()][$weapon]["k"] * 1; ?>
+					</td>
+					<td style="text-align: center;" <?php if (@$players[$player->getId()][$weapon]["d"] * 1 == 0) echo 'class="muted"'; ?>>
+						<?php echo @$players[$player->getId()][$weapon]["d"] * 1; ?>
+					</td>
+					<?php @$weaponsStats[$weapon] += @$players[$player->getId()][$weapon]["k"] * 1; ?>
 					<?php endforeach; ?>
 				</tr>
-			<?php endforeach; ?>
-		</tbody>
-		<tfoot>
-			<tr>
-				<td>Total</td>
-				<?php foreach ($pistols as $weapon): ?>
+				<?php endforeach; ?>
+			</tbody>
+			<tfoot>
+				<tr>
+					<td>Total</td>
+					<?php foreach ($pistols as $weapon): ?>
 					<td <?php if ($weaponsStats[$weapon] * 1 == 0) echo 'class="muted" '; ?> style="border-left: 2px solid #DDDDDD; text-align: center;" colspan="2"><?php echo $weaponsStats[$weapon] * 1; ?></td>
-				<?php endforeach; ?>
-			</tr>
-		</tfoot>
-	</table>
-</div>
-<div class="tab-pane" id="stats-weapon-heavy">
-	<table class="table">
-		<thead>
-			<tr>
-				<td rowspan="2"></td>
-				<?php foreach ($heavy as $weapon): ?>
+					<?php endforeach; ?>
+				</tr>
+			</tfoot>
+		</table>
+	</div>
+	<div class="tab-pane" id="stats-weapon-heavy">
+		<table class="table">
+			<thead>
+				<tr>
+					<td rowspan="2"></td>
+					<?php foreach ($heavy as $weapon): ?>
 					<td style="border-left: 1px solid #DDDDDD; text-align: center; min-width: 50px;" colspan="2"><?php echo image_tag("/images/kills/csgo/" . $weapon . ".png", array("class" => "needTips_S", "title" => $weapon)); ?></td>
-				<?php endforeach; ?>
-			</tr>
-			<tr>
-				<?php foreach ($heavy as $weapon): ?>
+					<?php endforeach; ?>
+				</tr>
+				<tr>
+					<?php foreach ($heavy as $weapon): ?>
 					<td style="border-left: 2px solid #DDDDDD;text-align: center;font-size: 10px; border-right: 1px solid #EEEEEE;">K</td>
 					<td style="font-size: 10px; text-align: center;">D</td>
-				<?php endforeach; ?>
-			</tr>
-		</thead>
-		<tbody>
-			<?php $weaponsStats = array(); ?>
-			<?php foreach ($match->getMap()->getPlayer() as $player): ?>
+					<?php endforeach; ?>
+				</tr>
+			</thead>
+			<tbody>
+				<?php $weaponsStats = array(); ?>
+				<?php foreach ($match->getMap()->getPlayer() as $player): ?>
 				<?php if ($player->getTeam() == "other") continue; ?>
 				<tr>
 					<td style="width: 150px; min-width: 150px;"><a href="<?php echo url_for("player_stats", array("id" => $player->getSteamid())); ?>"><?php echo $player->getPseudo(); ?></a></td>
 					<?php foreach ($heavy as $weapon): ?>
-						<td style="text-align: center;border-left: 2px solid #DDDDDD; border-right: 1px solid #EEEEEE;" <?php if (@$players[$player->getId()][$weapon]["k"] * 1 == 0) echo 'class="muted"'; ?>>
-							<?php echo @$players[$player->getId()][$weapon]["k"] * 1; ?>
-						</td>
-						<td style="text-align: center;" <?php if (@$players[$player->getId()][$weapon]["d"] * 1 == 0) echo 'class="muted"'; ?>>
-							<?php echo @$players[$player->getId()][$weapon]["d"] * 1; ?>
-						</td>
-						<?php @$weaponsStats[$weapon] += @$players[$player->getId()][$weapon]["k"] * 1; ?>
+					<td style="text-align: center;border-left: 2px solid #DDDDDD; border-right: 1px solid #EEEEEE;" <?php if (@$players[$player->getId()][$weapon]["k"] * 1 == 0) echo 'class="muted"'; ?>>
+						<?php echo @$players[$player->getId()][$weapon]["k"] * 1; ?>
+					</td>
+					<td style="text-align: center;" <?php if (@$players[$player->getId()][$weapon]["d"] * 1 == 0) echo 'class="muted"'; ?>>
+						<?php echo @$players[$player->getId()][$weapon]["d"] * 1; ?>
+					</td>	
+					<?php @$weaponsStats[$weapon] += @$players[$player->getId()][$weapon]["k"] * 1; ?>
 					<?php endforeach; ?>
 				</tr>
-			<?php endforeach; ?>
-		</tbody>
-		<tfoot>
-			<tr>
-				<td>Total</td>
-				<?php foreach ($heavy as $weapon): ?>
-					<td <?php if ($weaponsStats[$weapon] * 1 == 0) echo 'class="muted" '; ?> style="border-left: 2px solid #DDDDDD; text-align: center;" colspan="2"><?php echo $weaponsStats[$weapon] * 1; ?></td>
 				<?php endforeach; ?>
-			</tr>
-		</tfoot>
-	</table>
-</div>
-<div class="tab-pane" id="stats-weapon-smg">
-	<table class="table">
-		<thead>
-			<tr>
-				<td rowspan="2"></td>
-				<?php foreach ($smg as $weapon): ?>
-					<td style="border-left: 1px solid #DDDDDD; text-align: center; min-width: 50px;" colspan="2"><?php echo image_tag("/images/kills/csgo/" . $weapon . ".png", array("class" => "needTips_S", "title" => $weapon)); ?></td>
-				<?php endforeach; ?>
-			</tr>
-			<tr>
-				<?php foreach ($smg as $weapon): ?>
-					<td style="border-left: 2px solid #DDDDDD;text-align: center;font-size: 10px; border-right: 1px solid #EEEEEE;">K</td>
-					<td style="font-size: 10px; text-align: center;">D</td>
-				<?php endforeach; ?>
-			</tr>
-		</thead>
-		<tbody>
-			<?php $weaponsStats = array(); ?>
-			<?php foreach ($match->getMap()->getPlayer() as $player): ?>
-				<?php if ($player->getTeam() == "other") continue; ?>
+			</tbody>
+			<tfoot>
 				<tr>
-					<td style="width: 150px; min-width: 150px;"><a href="<?php echo url_for("player_stats", array("id" => $player->getSteamid())); ?>"><?php echo $player->getPseudo(); ?></a></td>
+					<td>Total</td>
+					<?php foreach ($heavy as $weapon): ?>
+					<td <?php if ($weaponsStats[$weapon] * 1 == 0) echo 'class="muted" '; ?> style="border-left: 2px solid #DDDDDD; text-align: center;" colspan="2"><?php echo $weaponsStats[$weapon] * 1; ?></td>
+					<?php endforeach; ?>
+				</tr>
+			</tfoot>
+		</table>
+	</div>
+	<div class="tab-pane" id="stats-weapon-smg">
+		<table class="table">
+			<thead>
+				<tr>
+					<td rowspan="2"></td>
 					<?php foreach ($smg as $weapon): ?>
-						<td style="text-align: center;border-left: 2px solid #DDDDDD; border-right: 1px solid #EEEEEE;" <?php if (@$players[$player->getId()][$weapon]["k"] * 1 == 0) echo 'class="muted"'; ?>>
-							<?php echo @$players[$player->getId()][$weapon]["k"] * 1; ?>
-						</td>
-						<td style="text-align: center;" <?php if (@$players[$player->getId()][$weapon]["d"] * 1 == 0) echo 'class="muted"'; ?>>
-							<?php echo @$players[$player->getId()][$weapon]["d"] * 1; ?>
-						</td>
-						<?php @$weaponsStats[$weapon] += @$players[$player->getId()][$weapon]["k"] * 1; ?>
+					<td style="border-left: 1px solid #DDDDDD; text-align: center; min-width: 50px;" colspan="2"><?php echo image_tag("/images/kills/csgo/" . $weapon . ".png", array("class" => "needTips_S", "title" => $weapon)); ?></td>
 					<?php endforeach; ?>
 				</tr>
-			<?php endforeach; ?>
-		</tbody>
-		<tfoot>
-			<tr>
+				<tr>
+				<?php foreach ($smg as $weapon): ?>
+				<td style="border-left: 2px solid #DDDDDD;text-align: center;font-size: 10px; border-right: 1px solid #EEEEEE;">K</td>
+				<td style="font-size: 10px; text-align: center;">D</td>
+				<?php endforeach; ?>
+				</tr>
+			</thead>
+			<tbody>
+				<?php $weaponsStats = array(); ?>
+				<?php foreach ($match->getMap()->getPlayer() as $player): ?>
+				<?php if ($player->getTeam() == "other") continue; ?>
+				<tr>
+				<td style="width: 150px; min-width: 150px;"><a href="<?php echo url_for("player_stats", array("id" => $player->getSteamid())); ?>"><?php echo $player->getPseudo(); ?></a></td>
+				<?php foreach ($smg as $weapon): ?>
+				<td style="text-align: center;border-left: 2px solid #DDDDDD; border-right: 1px solid #EEEEEE;" <?php if (@$players[$player->getId()][$weapon]["k"] * 1 == 0) echo 'class="muted"'; ?>>
+				<?php echo @$players[$player->getId()][$weapon]["k"] * 1; ?>
+				</td>
+				<td style="text-align: center;" <?php if (@$players[$player->getId()][$weapon]["d"] * 1 == 0) echo 'class="muted"'; ?>>
+				<?php echo @$players[$player->getId()][$weapon]["d"] * 1; ?>
+				</td>
+				<?php @$weaponsStats[$weapon] += @$players[$player->getId()][$weapon]["k"] * 1; ?>
+				<?php endforeach; ?>
+				</tr>
+				<?php endforeach; ?>
+			</tbody>
+			<tfoot>
+				<tr>
 				<td>Total</td>
 				<?php foreach ($smg as $weapon): ?>
-					<td <?php if ($weaponsStats[$weapon] * 1 == 0) echo 'class="muted" '; ?> style="border-left: 2px solid #DDDDDD; text-align: center;" colspan="2"><?php echo $weaponsStats[$weapon] * 1; ?></td>
+				<td <?php if ($weaponsStats[$weapon] * 1 == 0) echo 'class="muted" '; ?> style="border-left: 2px solid #DDDDDD; text-align: center;" colspan="2"><?php echo $weaponsStats[$weapon] * 1; ?></td>
 				<?php endforeach; ?>
-			</tr>
-		</tfoot>
-	</table>
-</div>
-<div class="tab-pane" id="stats-weapon-rifles">
-	<table class="table">
-		<thead>
-			<tr>
+				</tr>
+			</tfoot>
+		</table>
+	</div>
+	<div class="tab-pane" id="stats-weapon-rifles">
+		<table class="table">
+			<thead>
+				<tr>
 				<td rowspan="2"></td>
 				<?php foreach ($rifles as $weapon): ?>
-					<td style="border-left: 1px solid #DDDDDD; text-align: center; min-width: 50px;" colspan="2"><?php echo image_tag("/images/kills/csgo/" . $weapon . ".png", array("class" => "needTips_S", "title" => $weapon)); ?></td>
+				<td style="border-left: 1px solid #DDDDDD; text-align: center; min-width: 50px;" colspan="2"><?php echo image_tag("/images/kills/csgo/" . $weapon . ".png", array("class" => "needTips_S", "title" => $weapon)); ?></td>
 				<?php endforeach; ?>
-			</tr>
-			<tr>
+				</tr>
+				<tr>
 				<?php foreach ($rifles as $weapon): ?>
-					<td style="border-left: 2px solid #DDDDDD;text-align: center;font-size: 10px; border-right: 1px solid #EEEEEE;">K</td>
-					<td style="font-size: 10px; text-align: center;">D</td>
+				<td style="border-left: 2px solid #DDDDDD;text-align: center;font-size: 10px; border-right: 1px solid #EEEEEE;">K</td>
+				<td style="font-size: 10px; text-align: center;">D</td>
 				<?php endforeach; ?>
-			</tr>
-		</thead>
-		<tbody>
-			<?php $weaponsStats = array(); ?>
-			<?php foreach ($match->getMap()->getPlayer() as $player): ?>
+				</tr>
+			</thead>
+			<tbody>
+				<?php $weaponsStats = array(); ?>
+				<?php foreach ($match->getMap()->getPlayer() as $player): ?>
 				<?php if ($player->getTeam() == "other") continue; ?>
 				<tr>
-					<td style="width: 150px; min-width: 150px;"><a href="<?php echo url_for("player_stats", array("id" => $player->getSteamid())); ?>"><?php echo $player->getPseudo(); ?></a></td>
-					<?php foreach ($rifles as $weapon): ?>
-						<td style="text-align: center;border-left: 2px solid #DDDDDD; border-right: 1px solid #EEEEEE;" <?php if (@$players[$player->getId()][$weapon]["k"] * 1 == 0) echo 'class="muted"'; ?>>
-							<?php echo @$players[$player->getId()][$weapon]["k"] * 1; ?>
-						</td>
-						<td style="text-align: center;" <?php if (@$players[$player->getId()][$weapon]["d"] * 1 == 0) echo 'class="muted"'; ?>>
-							<?php echo @$players[$player->getId()][$weapon]["d"] * 1; ?>
-						</td>
-						<?php @$weaponsStats[$weapon] += @$players[$player->getId()][$weapon]["k"] * 1; ?>
-					<?php endforeach; ?>
+				<td style="width: 150px; min-width: 150px;"><a href="<?php echo url_for("player_stats", array("id" => $player->getSteamid())); ?>"><?php echo $player->getPseudo(); ?></a></td>
+				<?php foreach ($rifles as $weapon): ?>
+				<td style="text-align: center;border-left: 2px solid #DDDDDD; border-right: 1px solid #EEEEEE;" <?php if (@$players[$player->getId()][$weapon]["k"] * 1 == 0) echo 'class="muted"'; ?>>
+				<?php echo @$players[$player->getId()][$weapon]["k"] * 1; ?>
+				</td>
+				<td style="text-align: center;" <?php if (@$players[$player->getId()][$weapon]["d"] * 1 == 0) echo 'class="muted"'; ?>>
+				<?php echo @$players[$player->getId()][$weapon]["d"] * 1; ?>
+				</td>
+				<?php @$weaponsStats[$weapon] += @$players[$player->getId()][$weapon]["k"] * 1; ?>
+				<?php endforeach; ?>
 				</tr>
-			<?php endforeach; ?>
-		</tbody>
-		<tfoot>
-			<tr>
+				<?php endforeach; ?>
+			</tbody>
+			<tfoot>
+				<tr>
 				<td>Total</td>
 				<?php foreach ($rifles as $weapon): ?>
-					<td <?php if ($weaponsStats[$weapon] * 1 == 0) echo 'class="muted" '; ?> style="border-left: 2px solid #DDDDDD; text-align: center;" colspan="2"><?php echo $weaponsStats[$weapon] * 1; ?></td>
+				<td <?php if ($weaponsStats[$weapon] * 1 == 0) echo 'class="muted" '; ?> style="border-left: 2px solid #DDDDDD; text-align: center;" colspan="2"><?php echo $weaponsStats[$weapon] * 1; ?></td>
 				<?php endforeach; ?>
-			</tr>
-		</tfoot>
-	</table>
-</div>
-<div class="tab-pane" id="stats-weapon-equipment">
-	<table class="table">
-		<thead>
-			<tr>
+				</tr>
+			</tfoot>
+		</table>
+	</div>
+	<div class="tab-pane" id="stats-weapon-equipment">
+		<table class="table">
+			<thead>
+				<tr>
 				<td rowspan="2"></td>
 				<?php foreach ($equipment as $weapon): ?>
-					<td style="border-left: 1px solid #DDDDDD; text-align: center; min-width: 50px;" colspan="2"><?php echo image_tag("/images/kills/csgo/" . $weapon . ".png", array("class" => "needTips_S", "title" => $weapon)); ?></td>
+				<td style="border-left: 1px solid #DDDDDD; text-align: center; min-width: 50px;" colspan="2"><?php echo image_tag("/images/kills/csgo/" . $weapon . ".png", array("class" => "needTips_S", "title" => $weapon)); ?></td>
 				<?php endforeach; ?>
-			</tr>
-			<tr>
+				</tr>
+				<tr>
 				<?php foreach ($equipment as $weapon): ?>
-					<td style="border-left: 2px solid #DDDDDD;text-align: center;font-size: 10px; border-right: 1px solid #EEEEEE;">K</td>
-					<td style="font-size: 10px; text-align: center;">D</td>
+				<td style="border-left: 2px solid #DDDDDD;text-align: center;font-size: 10px; border-right: 1px solid #EEEEEE;">K</td>
+				<td style="font-size: 10px; text-align: center;">D</td>
 				<?php endforeach; ?>
-			</tr>
-		</thead>
-		<tbody>
-			<?php $weaponsStats = array(); ?>
-			<?php foreach ($match->getMap()->getPlayer() as $player): ?>
+				</tr>
+			</thead>
+			<tbody>
+				<?php $weaponsStats = array(); ?>
+				<?php foreach ($match->getMap()->getPlayer() as $player): ?>
 				<?php if ($player->getTeam() == "other") continue; ?>
 				<tr>
-					<td style="width: 150px; min-width: 150px;"><a href="<?php echo url_for("player_stats", array("id" => $player->getSteamid())); ?>"><?php echo $player->getPseudo(); ?></a></td>
-					<?php foreach ($equipment as $weapon): ?>
-						<td style="text-align: center;border-left: 2px solid #DDDDDD; border-right: 1px solid #EEEEEE;" <?php if (@$players[$player->getId()][$weapon]["k"] * 1 == 0) echo 'class="muted"'; ?>>
-							<?php echo @$players[$player->getId()][$weapon]["k"] * 1; ?>
-						</td>
-						<td style="text-align: center;" <?php if (@$players[$player->getId()][$weapon]["d"] * 1 == 0) echo 'class="muted"'; ?>>
-							<?php echo @$players[$player->getId()][$weapon]["d"] * 1; ?>
-						</td>
-						<?php @$weaponsStats[$weapon] += @$players[$player->getId()][$weapon]["k"] * 1; ?>
-					<?php endforeach; ?>
+				<td style="width: 150px; min-width: 150px;"><a href="<?php echo url_for("player_stats", array("id" => $player->getSteamid())); ?>"><?php echo $player->getPseudo(); ?></a></td>
+				<?php foreach ($equipment as $weapon): ?>
+				<td style="text-align: center;border-left: 2px solid #DDDDDD; border-right: 1px solid #EEEEEE;" <?php if (@$players[$player->getId()][$weapon]["k"] * 1 == 0) echo 'class="muted"'; ?>>
+				<?php echo @$players[$player->getId()][$weapon]["k"] * 1; ?>
+				</td>
+				<td style="text-align: center;" <?php if (@$players[$player->getId()][$weapon]["d"] * 1 == 0) echo 'class="muted"'; ?>>
+				<?php echo @$players[$player->getId()][$weapon]["d"] * 1; ?>
+				</td>
+				<?php @$weaponsStats[$weapon] += @$players[$player->getId()][$weapon]["k"] * 1; ?>
+				<?php endforeach; ?>
 				</tr>
-			<?php endforeach; ?>
-		</tbody>
-		<tfoot>
-			<tr>
+				<?php endforeach; ?>
+			</tbody>
+			<tfoot>
+				<tr>
 				<td>Total</td>
 				<?php foreach ($equipment as $weapon): ?>
-					<td <?php if ($weaponsStats[$weapon] * 1 == 0) echo 'class="muted" '; ?> style="border-left: 2px solid #DDDDDD; text-align: center;" colspan="2"><?php echo $weaponsStats[$weapon] * 1; ?></td>
+				<td <?php if ($weaponsStats[$weapon] * 1 == 0) echo 'class="muted" '; ?> style="border-left: 2px solid #DDDDDD; text-align: center;" colspan="2"><?php echo $weaponsStats[$weapon] * 1; ?></td>
 				<?php endforeach; ?>
-			</tr>
-		</tfoot>
-	</table>
+				</tr>
+			</tfoot>
+		</table>
+	</div>
 </div>
-
 <h5><i class="icon-info-sign"></i> <?php echo __("Info"); ?></h5>
 <div class="well">
-    <?php echo __("The column <b>K</b> represents the number of kill with this weapons, the column <b>D</b> represents the number of times the player has been killed by the weapon"); ?>
+	<?php echo __("The column <b>K</b> represents the number of kill with this weapons, the column <b>D</b> represents the number of times the player has been killed by the weapon"); ?>
 </div>
